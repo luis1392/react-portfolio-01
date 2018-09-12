a518e5017722571482067c83ca23f2f05708ef32
Luis Enrique Tierrafria Rodriguez <luis@Luiss-MacBook-Pro.local>
2018-09-12 15:56:01 -0500

    fix code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import PortfolioFixed from './components/PortfolioFixed'
-import PortfolioContent from './components/PortfolioContent'
-import SkillsList from './components/SkillsList'
-import EducationHistory from './components/EducationHistory'
-import WorkHistory from './components/WorkHistory'
-
+import React, { Component } from "react";
+import PortfolioFixed from "./components/PortfolioFixed";
+import PortfolioContent from "./components/PortfolioContent";
+import SkillsList from "./components/SkillsList";
+import EducationHistory from "./components/EducationHistory";
+import WorkHistory from "./components/WorkHistory";
 
 class App extends Component {
   render() {
-    const skills  = this.props.skills
-    const eduList = this.props.eduList
-    const jobsList = this.props.jobsList
     return (
       <div className="app-container">
-        <PortfolioFixed title='María Santiago' subtitle='Developer | Mother' image='https://s3.amazonaws.com/uifaces/faces/twitter/jina/128.jpg'/>
-        <PortfolioContent skills={skills} eduList={eduList} jobsList={jobsList}/>
-
+        <PortfolioFixed
+          title="María Santiago"
+          subtitle="Developer | Mother"
+          image="https://s3.amazonaws.com/uifaces/faces/twitter/jina/128.jpg"
+        />
+        <PortfolioContent
+          skills={this.props.skills}
+          eduList={this.props.eduList}
+          jobsList={this.props.jobsList}
+        />
       </div>
     );
   }
